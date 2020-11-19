edd70af86038009b59e2cbcec6a422f7cc15fab3
Mhurta41 <Mhurta41@gmail.com>
2020-11-19 12:21:06 -0600

    attempt 1 at modal

--- src/components/PlantDetails/PlantDetails.js
+++ src/components/PlantDetails/PlantDetails.js
@@ -1,7 +1,123 @@
 import React, { useState, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import Axios from 'axios';
 import './plantDetails.css';
 
+const UpdateModal = () => {
+	const [form, setForm] = useState(false);
+	const handleClose = () => setForm(false);
+	const handleShow = () => setForm(true);
+
+	const initialState = {
+		name: '',
+		family: '',
+		commonName: '',
+		genus: '',
+		scientificName: '',
+	};
+
+	const handlePut = function () {
+		const data = {
+			common_name: 'Planty',
+			slug: 'planties slug',
+		};
+		//Put request to update to specific plant id page
+		Axios.put(
+			`https://botanical-babble.herokuapp.com/api/plants/${plantId}`,
+			data
+		).then((response) => console.log(response));
+	};
+	//To submit changes to your plant details
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// setFormState(initialState);
+		handlePut();
+	};
+
+	const handleChange = (event) => {
+		setFormState({ ...formState, [event.target.id]: event.target.value });
+	};
+
+	const [formState, setFormState] = useState(initialState);
+
+	return (
+		<>
+			<Modal show={form} onHide={handleClose} centered size='md'>
+				<Modal.Header>
+					<Modal.Title>Edit Plant</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<form action='submit'>
+						{/* Name */}
+						<label htmlFor='name'>Name:</label>
+						<input
+							type='text'
+							name=''
+							id='name'
+							placeholder='Name'
+							onChange={handleChange}
+						/>
+						<br />
+						{/* Family */}
+						<label htmlFor='family'>Family:</label>
+						<input
+							type='text'
+							name=''
+							id='family'
+							placeholder='Family'
+							onChange={handleChange}
+						/>
+						<br />
+						{/* Common Name */}
+						<label htmlFor='commonName'>Common Name:</label>
+						<input
+							type='text'
+							name=''
+							id='commonName'
+							placeholder='Family Common Name'
+							onChange={handleChange}
+						/>
+						<br />
+						{/* Genus */}
+						<label htmlFor='genus'>Genus:</label>
+						<input
+							type='text'
+							name=''
+							id='genus'
+							placeholder='Genus'
+							onChange={handleChange}
+						/>
+						<br />
+						{/* Scientific Name */}
+						<label htmlFor='scientificName'>Scientific Name:</label>
+						<input
+							type='text'
+							name=''
+							id='scientificName'
+							placeholder='Scientific Name'
+							onChange={handleChange}
+						/>
+					</form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant='secondary' onClick={handleClose}>
+						Close
+					</Button>
+					<Button
+						variant='primary'
+						onClick={() => {
+							handleClose();
+							handleSubmit();
+						}}>
+						update changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+};
+//Created a container to hold individual plant profile
 const PlantDetails = ({ match }) => {
 	const plantId = match.params._id;
 	const [plant, setPlant] = useState([]);
@@ -44,7 +160,10 @@ const PlantDetails = ({ match }) => {
 				irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
 				fugiat nulla pariatur.
 			</p>
+			<button onClick={handleShow}>Edit plant</button>
 		</section>
+		//Created button above to edit and update plant details
 	);
 };
+
 export default PlantDetails;
